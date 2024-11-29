8d8584d446aca7c9f0ad1cba9abd1bacb1f924e9
khangtruong2252314 <khang.truong2252314@hcmut.edu.vn>
2024-11-29 21:24:57 +0700

    modified:   box/magnet.json 	modified:   box/meta.torrent.json 	modified:   start_client.py

--- start_client.py
+++ start_client.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--request_files', type=str, nargs='+', default=[])
     parser.add_argument('--tracker_ip', type=str, default='127.0.0.1')
     parser.add_argument('--tracker_port', type=int, default=1108)
+    parser.add_argument('--peer_ip', type=str, default='127.0.0.1')
     args = parser.parse_args()
     tracker_port = args.port
     peer_port = args.port
@@ -18,9 +19,10 @@ if __name__ == '__main__':
     tracker_ip = args.tracker_ip
     tracker_port = args.tracker_port
     peer_name = args.peer_name
+    peer_ip = args.peer_ip
 
     asyncio.run(run_peer(
-        peer_ip='127.0.0.1',
+        peer_ip=peer_ip,
         tracker_ip=tracker_ip, 
         tracker_port=tracker_port, 
         peer_port=peer_port, 
