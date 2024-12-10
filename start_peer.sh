eeec29594ef9e279f241645c5895ebb286b19c3f
khangtruong2252314 <khang.truong2252314@hcmut.edu.vn>
2024-12-10 11:30:59 +0700

    modified:   start_peer.sh

--- start_peer.sh
+++ start_peer.sh
@@ -1,4 +1,4 @@
 python3 start_client.py --tracker_port 1108 --peer_name PEER_A --port 1109 --tracker_ip 192.168.2.17
-python3 start_client.py --tracker_port 1108 --peer_name PEER_A --port 1109 --tracker_ip 192.168.2.17 --request_files "A.png"
-python3 start_client.py --tracker_port 1108 --peer_name PEER_A --port 1109 --tracker_ip 192.168.2.17 --request_files "A.png" "B.png"
+python3 start_client.py --tracker_port 1108 --peer_name PEER_B --port 1110 --tracker_ip 192.168.2.17 --request_files "A.png"
+python3 start_client.py --tracker_port 1108 --peer_name PEER_C --port 1111 --tracker_ip 192.168.2.17 --request_files "A.png" "B.png"
 
